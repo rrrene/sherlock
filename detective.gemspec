8f2c61bf249edc2d7f5722be2e12b23ae104ce46
René Föhring <rene@bamaru.de>
2011-02-10 08:21:20 +0100

    Added VERSION constant and gemspec

---
+++ detective.gemspec
@@ -0,0 +1,21 @@
+$:.unshift('lib')
+require 'detective'
+
+Gem::Specification.new do |s|
+  s.author = "René Föhring"
+  s.email = 'rf@bamaru.de'
+  s.homepage = "http://bamaru.com"
+
+  s.name = 'detective'
+  s.version = Detective::VERSION::STRING
+  s.platform = Gem::Platform::RUBY
+  s.summary = "A library for filtering lists of files and performing actions on their content."
+  s.description = "A library for filtering lists of files and performing actions on their content."
+
+  s.files = Dir[ 'lib/**/*', 'spec/**/*']
+  s.has_rdoc = false
+  s.require_path = 'lib'
+  s.requirements << 'none'
+
+  s.add_dependency('rspec', '~> 1.3.0')
+end
--- lib/detective.rb
+++ lib/detective.rb
@@ -1,22 +1,21 @@
 #!/usr/bin/env ruby -wKU
 
-# TODO: Vielleicht Möglichkeit 'gsub' auf ganze FileCollection zu machen (wird einfach auf File.read(f) angewendet).
-# TODO: Möglichkeit MatchedLine Objekte zu ändern und in Datei zurückzuspeichern.
-# TODO: 'rewrite' auf LineCollection, die genau das tut.
-#       -> muss natürlich rückwärts die LineCollection durchlaufen, damit sich Zeilennummern nicht ändern...
-#   investigate(/literatur/).collect(/\\quelle\{(.+\(\d{4}\))\}[\s\\]+/).rewrite { |line| "%" + line }
 # TODO: Output-Methoden für File- und Line-Collections, Tabellen, success & failure inkl. optionaler Colorierung.
 # TODO: vielleicht LaTex shortcuts à la tex_files.inputs(:only => 'generated/schema')
-
-# DONE: Möglichkeit 'gsub' auf ganze LineCollection zu machen (wird nicht direkt gespeichert, sondern kann weiter verwendet werden).
-# DONE: Methoden wie 'first', '+', '-' etc. für LineCollection adaptieren.
-
 
 %w(collection core_ext latex).each do |_module|
   require File.join(File.dirname(__FILE__), _module)
 end
 
 module Detective
+  module VERSION #:nodoc:
+    MAJOR = 0
+    MINOR = 1
+    BUILD = 0
+
+    STRING = [MAJOR, MINOR, BUILD].join('.').freeze
+  end
+  
   module InstanceMethods
     def collect_files_matching(*args)
       Detective::Collection::Files.new(*args)
