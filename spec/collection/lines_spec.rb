a30b7d0ed6acc804e1eef6efe1d8dad90fcf8ef0
René Föhring <rf@bamaru.de>
2011-03-02 15:33:35 +0100

    on tests: Files#collect -> Files#lines

--- spec/collection/lines_spec.rb
+++ spec/collection/lines_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Sherlock::Collection::Files do
   def numbered_lines
-    text_files(:only => /lines/).collect(/^.*(\d+\.)(.+)/)
+    text_files(:only => /lines/).lines(/^.*(\d+\.)(.+)/)
   end
   
   describe "#initialize" do
--- spec/collection/matched_line_spec.rb
+++ spec/collection/matched_line_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Sherlock::Collection::Files do
   def some_line
-    lines = text_files.collect('Zeile')
+    lines = text_files.lines('Zeile')
     lines[0]
   end
   
